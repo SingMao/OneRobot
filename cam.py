ec0e343aa311ca94d272910932b75c58f7208cdb
lac <lac@gmail.com>
2016-08-08 22:11:56 +0800

    uncommited

--- cam.py
+++ cam.py
@@ -4,8 +4,8 @@ import os
 import pyuarm
 import time
 import numpy as np
+from sk import ArmCalibrate
 
-cap = cv2.VideoCapture(0)
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 12800)
 # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 7200)
 # cap.set(cv2.CAP_PROP_FPS, 1)
@@ -18,11 +18,7 @@ cap = cv2.VideoCapture(0)
 # cap.set(cv2.CAP_PROP_SHARPNESS, 0.2)
 # cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, False)
 
-cap.read()
-
-os.system('v4l2-ctl -c exposure_absolute=20')
-
-def get_maxpt():
+def get_maxpt(cap):
     ok, img = cap.read()
     # print(img.shape if ok else 'Q__Q')
     if not ok:
@@ -33,7 +29,7 @@ def get_maxpt():
     if maxval >= 140:
         cv2.circle(img, maxpos, 20, (0, 255, 0), 5)
     else:
-        return None
+        maxpos = None
 
     cv2.imshow('test', img)
     cv2.waitKey(10)
@@ -43,52 +39,67 @@ uarm = pyuarm.uArm()
 uarm.attach_all_servos()
 
 uarm.write_servo_angle(pyuarm.SERVO_BOTTOM, 90, False)
-uarm.write_servo_angle(pyuarm.SERVO_LEFT, 70, False)
+uarm.write_servo_angle(pyuarm.SERVO_LEFT, 90, False)
 uarm.write_servo_angle(pyuarm.SERVO_RIGHT, 30, False)
 
-print(uarm.read_servo_angle()[:3])
+AC = ArmCalibrate()
 
-angles = []
-pts = []
+def writeBR(b, r):
+    uarm.write_servo_angle(pyuarm.SERVO_BOTTOM, b, False)
+    uarm.write_servo_angle(pyuarm.SERVO_RIGHT, r, False)
 
-for i in range(200):
-    if i % 10 == 0:
-        print(i)
-    phi = np.random.randint(75, 105)
-    theta = np.random.randint(10, 40)
-    # phi = 75 + i
-    # theta = 10 + i
-    uarm.write_servo_angle(pyuarm.SERVO_BOTTOM, phi, False)
-    uarm.write_servo_angle(pyuarm.SERVO_RIGHT, theta, False)
-    t0 = time.time()
-    while time.time() < t0 + 1.:
-        get_maxpt()
-        cv2.waitKey(10)
-    ang = np.mean([np.array(uarm.read_servo_angle()[:3]) for x in range(4)], axis=0)
-    maxpos = get_maxpt()
-    if maxpos is not None:
-        angles.append(ang)
-        pts.append(maxpos)
+def calibrate(cap):
+    cap.read()
 
-angles = np.array(angles)
-pts = np.array(pts)
-np.savez('angs.npz', angles=angles, pts=pts)
+    os.system('v4l2-ctl -c exposure_auto=1')
+    os.system('v4l2-ctl -c exposure_absolute=20')
 
-print('Complete')
-exit()
+    angles = []
+    pts = []
 
-while True:
-    maxpos = get_maxpt()
+    for i in range(10):
+        phi = np.random.randint(75, 100)
+        theta = np.random.randint(10, 35)
+        # phi = 75 + i
+        # theta = 10 + i
+        writeBR(phi, theta)
+        t0 = time.time()
+        while time.time() < t0 + 1.:
+            get_maxpt(cap)
+            cv2.waitKey(10)
+        ang = np.mean([np.array(uarm.read_servo_angle()[:3]) for x in range(4)], axis=0)
+        maxpos = get_maxpt(cap)
+        if maxpos is not None:
+            angles.append(ang)
+            pts.append(maxpos)
 
-# while True:
-    # ok, img = cap.read()
-    # # print(img.shape if ok else 'Q__Q')
-    # if not ok: continue
+    angles = np.array(angles)
+    pts = np.array(pts)
+    # np.savez('angs10.npz', angles=angles, pts=pts)
 
-    # _, maxval, _, maxpos = cv2.minMaxLoc(img[:,:,2])
-    # print(maxval, maxpos)
-    # if maxval >= 150:
-        # cv2.circle(img, maxpos, 20, (0, 255, 0), 5)
+    AC.fit(angles, pts)
 
-    # cv2.imshow('test', img)
-    # cv2.waitKey(10)
+    # for p, a in zip(pts, angles):
+        # print(p, AC.predict(a))
+
+    # for i in range(0, 601, 200):
+        # for j in range(0, 401, 200):
+            # phi, theta = AC.inv_predict((i, j))
+            # writeBR(phi, theta)
+            # t0 = time.time()
+            # while time.time() < t0 + 1.:
+                # get_maxpt(cap)
+                # cv2.waitKey(10)
+            # mpt = get_maxpt(cap)
+            # print(i, j, mpt, phi, theta, AC.predict((phi, 0, theta)))
+            # print(uarm.read_servo_angle()[:3], AC.predict(uarm.read_servo_angle()[:3]))
+
+    os.system('v4l2-ctl -c exposure_auto=3')
+
+    print('Calibration Completed')
+
+def move_to(x, y):
+    theta, phi = AC.inv_predict((x, y))
+    writeBR(theta, phi)
+    time.sleep(1.)
+    return AC.predict(uarm.read_servo_angle()[:3])
--- one.py
+++ one.py
@@ -6,6 +6,7 @@ import json
 import subprocess
 import os
 import fcntl
+import cam
 
 CELL_WIDTH = 32
 CELL_HEIGHT = 32
@@ -39,10 +40,10 @@ def normalize_image(img):
             real_contour = None
     
     if real_contour is None:
-        return None
+        return None, None
 
     if len(approx_contour) != 4:
-        return None
+        return None, None
 
     # cv2.drawContours(img, [real_contour], 0, (255, 0, 0), 10)
     # cv2.imshow('cont', img)
@@ -57,15 +58,15 @@ def normalize_image(img):
     pers_transform = cv2.getPerspectiveTransform(pers_src, pers_dst)
 
     dst = cv2.warpPerspective(img, pers_transform, (NEW_WIDTH, NEW_HEIGHT))
-    return dst
+    return dst, pers_transform
 
 def split_image(img, cw, ch, w, h):
     return [[img[i*cw:(i+1)*cw,j*ch:(j+1)*ch] for j in range(h)] for i in range(w)]
 
 cap = cv2.VideoCapture(0)
 # cap.set(cv2.cv.CV_CAP_PROP_FPS, 1)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 torch_process = subprocess.Popen(
     ['th', 'luas/classify.lua', 'model_best.t7', 'sis.npy'],
@@ -74,12 +75,14 @@ torch_process = subprocess.Popen(
 imgpath = '20160724-222718'
 imglist = [os.path.join(imgpath, x) for x in os.listdir(imgpath)]
 
+cam.calibrate(cap)
+
 cnt = 0
-while True:
+pers_transform = None
+while cnt < 5:
     try:
-        cnt += 1
         print('It %d' % cnt)
-        imgidx = cnt % len(imglist)
+        # imgidx = cnt % len(imglist)
         # img = cv2.imread(imglist[imgidx])
         img = cap.read()[1]
 
@@ -88,9 +91,10 @@ while True:
         cv2.imshow('original', img)
         cv2.waitKey(10)
 
-        dst = normalize_image(img)
+        dst, pers_transform = normalize_image(img)
         if dst is None:
             continue
+        cnt += 1
 
         means = np.mean(dst, (0, 1))
         std = np.std(dst, (0, 1))
@@ -125,3 +129,29 @@ while True:
     except KeyboardInterrupt:
         torch_process.terminate()
         break
+
+print('Good')
+print(pers_transform)
+inv_pers_transform = np.linalg.inv(pers_transform)
+
+def pTrans(s, m):
+    x, y = s
+    arr = np.array([[[x, y]]]).astype(float)
+    return cv2.perspectiveTransform(arr, m)[0][0]
+
+while True:
+    i = np.random.randint(0, 9)
+    j = np.random.randint(0, 9)
+    x = i * 32 + 16
+    y = j * 32 + 16
+    pt = pTrans((x, y), inv_pers_transform)
+    print(pt)
+    xx, yy = cam.move_to(*pt)
+    t0 = time.time()
+    while time.time() < t0 + 0.5:
+        ok, img = cap.read()
+        cv2.circle(img, (int(pt[0]), int(pt[1])), 5, (255, 0, 0), 2)
+        cv2.circle(img, (int(xx), int(yy)), 5, (0, 255, 0), 2)
+        cv2.imshow('test', img)
+        cv2.waitKey(10)
+

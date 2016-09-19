2d9114a1834ebbf1cf2999e9d158893a08d90535
tk1 <tk1@tk1.com>
2016-09-19 18:43:36 +0800

    test_hsv_sphere

--- Stage2/alphabet_data/feat.py
+++ Stage2/alphabet_data/feat.py
@@ -5,24 +5,16 @@ import sys
 import time
 import matplotlib.pyplot as plt
 
+HUE_R = 2.
+
 def img2huevec(iim):
     hsv = cv2.cvtColor(iim, cv2.COLOR_BGR2HSV)
     hue = hsv[:,:,0] / 180. * 2. * np.pi
+    sat = hsv[:,:,1] / 255.
     hvec = np.concatenate([
-        np.cos(hue)[:,:,np.newaxis],
-        np.sin(hue)[:,:,np.newaxis],
-    ], axis=2)
-    return hvec.astype(np.float32)
-
-def img2huevec_rand(iim):
-    hsv = cv2.cvtColor(iim, cv2.COLOR_BGR2HSV)
-    hue = hsv[:,:,0] / 180. * 2. * np.pi
-    val = np.minimum(hsv[:,:,2], 1)
-    rth = np.random.random(iim.shape[:-1]) * (2 * np.pi)
-    hue = hue * val + rth * (1-val)
-    hvec = np.concatenate([
-        np.cos(hue)[:,:,np.newaxis],
-        np.sin(hue)[:,:,np.newaxis],
+        (np.cos(hue) * sat)[:,:,np.newaxis],
+        (np.sin(hue) * sat)[:,:,np.newaxis],
+        (np.sqrt(HUE_R*HUE_R-sat*sat))[:,:,np.newaxis],
     ], axis=2)
     return hvec.astype(np.float32)
 
@@ -55,40 +47,41 @@ path = sys.argv[1] if len(sys.argv) >= 2 else 'da.png'
 
 cap = cv2.VideoCapture(0)
 
-tpl = cv2.imread('tpl_f.png', cv2.CV_LOAD_IMAGE_UNCHANGED)
-tpl, tpl_mask = process_template(tpl)
-orig_tpl = tpl
-
-tpls = [] #(r, theta, tpl, tpl_hue, otpl)
-tpls_mini = []
-
 shrink_ratio = 0.5
 
-for r in np.linspace(0.45, 0.45, 1):
-    for theta in np.linspace(0, 360, 36+1)[:-1]:
-        _tpl = rotate_scale(tpl, theta, r, r)
-        _otpl = rotate_scale(orig_tpl, theta, r, r)
-        _mask = rotate_scale(tpl_mask, theta, r, r)
-        _tpl = cv2.Canny(_tpl, 100, 200).astype(np.float32)
-        R = 1
-        _tpl = cv2.GaussianBlur(_tpl, (3*R, 3*R), R) / 40.
-        _tpl = np.minimum(_tpl, 1) - 0.8
-        _tpl = np.maximum(_tpl, -_mask)
-        _hue = img2huevec(_otpl) * _mask[:,:,np.newaxis]
+tpls = []
+tpls_mini = []
 
-        tpls.append((r, theta, _tpl, _hue, _otpl))
+alphabet = 'abcdef'
 
-        _tpl = cv2.resize(_tpl, (0, 0), None, shrink_ratio, shrink_ratio)
-        _otpl = cv2.resize(_otpl, (0, 0), None, shrink_ratio, shrink_ratio)
-        _hue = cv2.resize(_hue, (0, 0), None, shrink_ratio, shrink_ratio)
+for i, c in enumerate(alphabet):
+    tpl = cv2.imread('tpl_%s.png' % c, cv2.CV_LOAD_IMAGE_UNCHANGED)
+    tpl, tpl_mask = process_template(tpl)
+    orig_tpl = tpl
 
-        tpls_mini.append((r, theta, _tpl, _hue, _otpl))
+    tpls.append([]) #(r, theta, tpl, tpl_hue, otpl)
+    tpls_mini.append([])
 
-        # print(r, theta)
-        # cv2.imshow('tpl', _tpl)
-        # cv2.imshow('otpl', _otpl)
-        # cv2.imshow('mask', _mask)
-        # cv2.waitKey(100)
+
+    for r in np.linspace(0.45, 0.45, 1):
+        for theta in np.linspace(0, 360, 36+1)[:-1]:
+            _tpl = rotate_scale(tpl, theta, r, r)
+            _otpl = rotate_scale(orig_tpl, theta, r, r)
+            _mask = rotate_scale(tpl_mask, theta, r, r)
+            _tpl = cv2.Canny(_tpl, 100, 200).astype(np.float32)
+            R = 1
+            _tpl = cv2.GaussianBlur(_tpl, (3*R, 3*R), R) / 40.
+            _tpl = np.minimum(_tpl, 1) - 0.8
+            _tpl = np.maximum(_tpl, -_mask)
+            _hue = img2huevec(_otpl) * _mask[:,:,np.newaxis]
+
+            tpls[i].append((r, theta, _tpl, _hue, _otpl))
+
+            _tpl = cv2.resize(_tpl, (0, 0), None, shrink_ratio, shrink_ratio)
+            _otpl = cv2.resize(_otpl, (0, 0), None, shrink_ratio, shrink_ratio)
+            _hue = cv2.resize(_hue, (0, 0), None, shrink_ratio, shrink_ratio)
+
+            tpls_mini[i].append((r, theta, _tpl, _hue, _otpl))
 
 # exit(0)
 
@@ -110,11 +103,13 @@ pers_transform, mask = cv2.findHomography(p_src, p_dst)
 print(pers_transform)
 
 
-while True:
+for cnt in xrange(10**9):
     ok = False
     while not ok:
         ok, img = cap.read()
     # img = cv2.imread(path)
+    
+    idx = cnt % len(alphabet)
 
     # dst = cv2.warpPerspective(cal, pers_transform, (480, 540))
     img2 = cv2.warpPerspective(img, pers_transform, (480, 600))
@@ -122,12 +117,12 @@ while True:
     img2 = cv2.resize(img2, (0, 0), None, 0.5, 0.5)
     img2_canny = cv2.Canny(img2, 100, 200).astype(np.float32)
     img2_canny /= np.max(img2_canny)
-    img2_hue = img2huevec_rand(img2)
+    img2_hue = img2huevec(img2)
 
     img2_mini = cv2.resize(img2, (0, 0), None, shrink_ratio, shrink_ratio)
     img2_mini_canny = cv2.Canny(img2_mini, 100, 200).astype(np.float32)
     img2_mini_canny /= np.max(img2_mini_canny)
-    img2_mini_hue = img2huevec_rand(img2_mini)
+    img2_mini_hue = img2huevec(img2_mini)
 
     # cv2.imshow('img2', img2)
     cv2.imshow('img2_canny', img2_canny)
@@ -135,14 +130,15 @@ while True:
 
     candidates = [] #(val, r, th)
     t0 = time.time()
-    mp = np.zeros(img2.shape[:2])
 
-    for r, th, _tpl, _hue, _otpl in tpls_mini:
+    for r, th, _tpl, _hue, _otpl in tpls_mini[idx]:
         mat = cv2.matchTemplate(img2_mini_canny, _tpl, cv2.TM_CCORR_NORMED)
         total_sz = _hue.shape[0] * _hue.shape[1]
-        mat += (total_sz-cv2.matchTemplate(img2_mini_hue, _hue, cv2.TM_SQDIFF))  / total_sz * 0.3
+        nzcnt1 = np.sum(np.sum(_otpl, 2) != 0)
+        nzcnt = np.sum(_hue*_hue) / (HUE_R * HUE_R)
+        # print(th, nzcnt1, nzcnt, total_sz)
+        mat += (((total_sz-nzcnt) * HUE_R * HUE_R) - cv2.matchTemplate(img2_mini_hue, _hue, cv2.TM_SQDIFF)) / total_sz * 0.3
         a, b = mat.shape
-        mp[:a,:b] = np.maximum(mp[:a,:b], mat)
         _, Val, _, Loc = cv2.minMaxLoc(mat)
         Val = -Val
         candidates.append((Val, r, th))
@@ -161,13 +157,17 @@ while True:
     best_loc, bth, br = None, None, None
     btpl, botpl = None, None
     t0 = time.time()
-    mp = np.zeros(img2.shape[:2])
+    mp = np.ones(img2.shape[:2]) * (-1)
 
-    for r, th, _tpl, _hue, _otpl in tpls:
+    for r, th, _tpl, _hue, _otpl in tpls[idx]:
         if (r, th) not in candidates_set: continue
         mat = cv2.matchTemplate(img2_canny, _tpl, cv2.TM_CCORR_NORMED)
         total_sz = _hue.shape[0] * _hue.shape[1]
-        mat += (total_sz-cv2.matchTemplate(img2_hue, _hue, cv2.TM_SQDIFF))  / total_sz * 0.2
+        nzcnt = np.sum(np.sum(_otpl, 2) != 0)
+        nzcnt = np.sum(_hue*_hue) / (HUE_R * HUE_R)
+        cv2.imshow('ttt', np.sum(_otpl, 2).astype(np.float32))
+        cv2.waitKey(10)
+        mat += (((total_sz-nzcnt) * HUE_R * HUE_R) - cv2.matchTemplate(img2_hue, _hue, cv2.TM_SQDIFF)) / total_sz * 0.3
         # print(total_sz)
         a, b = mat.shape
         mp[:a,:b] = np.maximum(mp[:a,:b], mat)
@@ -176,6 +176,7 @@ while True:
         if Val < best_val:
             best_val, best_loc, bth, br = Val, Loc, th, r
             btpl, botpl = _tpl, _otpl
+
 
     print('Stage 2 Time : %.2f, Val : %.4f' % (time.time() - t0, best_val))
 
@@ -191,11 +192,17 @@ while True:
 
     ###
 
+    real_min = np.min((mp != -1) * mp)
+    mp = np.maximum(mp, real_min)
+
     mp = cv2.normalize(mp, None, 0., 1., cv2.NORM_MINMAX)
-    print(cv2.minMaxLoc(mp))
+    mp = (mp * 255).astype(np.uint8)
+    # print(cv2.minMaxLoc(mp))
+    mpjet = cv2.applyColorMap(mp, cv2.COLORMAP_JET)
+    print(mpjet.shape)
 
     while True:
-        cv2.imshow('mp', mp)
+        cv2.imshow('mp', mpjet)
         # cv2.imshow('tpl_r', tpl_r)
         cv2.imshow('tpl', btpl)
         cv2.imshow('otpl', botpl)
@@ -203,8 +210,8 @@ while True:
         cv2.imshow('img2', img2)
         # cv2.imshow('orig_img2', orig_img2)
         # cv2.imshow('img3', img3)
-        cv2.imshow('img2_hue0', img2_hue[:,:,0])
-        cv2.imshow('img2_hue1', img2_hue[:,:,1])
+        cv2.imshow('img2_hue0', (img2_hue[:,:,0]+1)/2)
+        cv2.imshow('img2_hue1', (img2_hue[:,:,1]+1)/2)
         # cv2.imshow('tpl_r_hue0', tpl_r_hue[:,:,0])
         # cv2.imshow('tpl_r_hue1', tpl_r_hue[:,:,1])
 
